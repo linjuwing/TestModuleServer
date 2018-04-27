0b01b4103acf9aad43e018cebece1beb8dbee1a0
linjuwing <ju.lin@prediktor.no>
2018-04-27 15:43:58 +0800

    first commit

---
+++ api/controllers/simple-action.js
@@ -0,0 +1,6 @@
+module.exports = async function simpleAction (req, res) {
+
+    // Display the welcome view, setting the view variable
+    // named "name" to the value of the user's name.
+    return res.send("good");
+};
